6560661b41c0e78229b87b646c33e51f200ac8dc
Humberto Dias <hldtux@gmail.com>
2024-01-18 13:17:57 -0300

    Home.tsx

---
+++ src/Home.tsx
@@ -0,0 +1,26 @@
+import GridClocks from "./GridClocks";
+
+function Home() {
+  // const timezones = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  const timezones = [
+    "Australia/Sydney",
+    "America/Sao_Paulo",
+    "America/New_York",
+    "Europe/London",
+  ];
+  // const timezones = Intl.supportedValuesOf('timeZone');
+
+  return (
+      <div>
+        <h1>The World Clock</h1>
+        <div className="card">
+          <GridClocks size={300} timezones={timezones} />
+        </div>
+        <div className="read-the-docs">
+          <a target="_blank" href={`https://github.com/humbertodias/react-timezone-clocks/commit/${import.meta.env.VITE_APP_VERSION}`}>{import.meta.env.VITE_APP_VERSION}</a>
+        </div>
+      </div>
+  );
+}
+
+export default Home;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,24 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import App from "./App.tsx";
+import Home from "./Home.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+const routes = [
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      { path: "/", element: <Home /> },
+    ],
+  },
+]
+const router = createBrowserRouter( routes
+  , { basename: import.meta.env.MODE == 'gh-pages' ? '/clocks/' : '/' } 
+  );
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </React.StrictMode>
+);
