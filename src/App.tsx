793208b538a6d6f013768e523098c68e6a390f0b
Humberto Dias <hldtux@gmail.com>
2024-01-18 09:09:45 -0300

    commit link

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,13 @@ import "./App.css";
 import GridClocks from "./GridClocks";
 
 function App() {
-
   // const timezones = Intl.DateTimeFormat().resolvedOptions().timeZone;
-  const timezones = ['Australia/Sydney', 'America/Sao_Paulo', 'America/New_York', 'Europe/London' ];
+  const timezones = [
+    "Australia/Sydney",
+    "America/Sao_Paulo",
+    "America/New_York",
+    "Europe/London",
+  ];
   // const timezones = Intl.supportedValuesOf('timeZone');
 
   return (
@@ -14,7 +18,11 @@ function App() {
         <div className="card">
           <GridClocks size={300} timezones={timezones} />
         </div>
-        <div className="read-the-docs">{import.meta.env.VITE_APP_VERSION}</div>
+        <div className="read-the-docs">
+          <a href={"https://github.com/humbertodias/react-timezone-clocks/commit/" + import.meta.env.VITE_APP_VERSION}>
+            {import.meta.env.VITE_APP_VERSION}
+          </a>
+        </div>
       </div>
     </>
   );
